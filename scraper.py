222bcc8326755402983b88aeec1af00d41ef8332
Locullus <bula.mickael@gmail.com>
2021-05-03 15:37:56 +0200

    regroupement de toutes les données scrapées dans une liste scraping_list

--- scraper.py
+++ scraper.py
@@ -27,29 +27,18 @@ row_xpath = '//*[@id="main-content"]/div/section[1]/div[2]/article/div[1]/div[2]
 # on détermine le nombre de données à récupérer en fonction de l'état du marché (ouvert ou fermé)
 index = 6 if opened else 7
 
-# on crée la boucle qui va scraper les dates
+# on crée les boucles qui vont scraper les différentes données
 date_list = [(get_data(content, date_xpath.format(i))+'-'+str(year)) for i in range(2, index)]
-print(date_list)
-
-# boucle qui va scraper les cours de fermeture  1 6
 closing_list = [get_data(content, row_xpath.format(1, i)) for i in range(2, index)]
-print(closing_list)
-
-# boucle qui va scraper les cours d'ouverture   3 6
 opening_list = [get_data(content, row_xpath.format(3, i)) for i in range(2, index)]
-print(opening_list)
-
-# boucle qui va scraper les plus haut   4 6
 higher_list = [get_data(content, row_xpath.format(4, i)) for i in range(2, index)]
-print(higher_list)
-
-# boucle qui va scraper les plus bas    5 6
 lower_list = [get_data(content, row_xpath.format(5, i)) for i in range(2, index)]
-print(lower_list)
 
-# on affiche les résultats
-print(f"date :      {date_list[-1]}")
-print(f"clôture :   {closing_list[-1]}")
-print(f"ouverture : {opening_list[-1]}")
-print(f"plus haut : {higher_list[-1]}")
-print(f"plus bas :  {lower_list[-1]}")
+# on crée la liste des données scrapées que l'on enverra à la base de données
+length = len(date_list)
+scraping_list = [[date_list[i], closing_list[i], opening_list[i], higher_list[i], lower_list[i]] for i in range(length)]
+
+# on affiche le résultat
+for element in scraping_list:
+    print(element)
+
