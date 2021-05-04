0d12d95d09e0dbc5d5c79a2b1cd1ff3134069f49
Locullus <bula.mickael@gmail.com>
2021-05-04 13:09:21 +0200

    Tos les appels à la base de données pour mettre à jour les données passent par la fonction stocks_bd_update(). récupération du nom de la table dans la présentation des résultats (l.19 session.py).

--- session.py
+++ session.py
@@ -5,60 +5,18 @@ from scraper import scraping_list_CAC, scraping_list_LVC, scraping_list_BX4
 
 Session = sessionmaker(bind=engine)
 
-# on ouvre une session pour récupérer la date la plus récente de la base de données
-session = Session()
-last_high = str(session.query(CAC.date).order_by(CAC.id.desc()).first())
-print(f"La date la plus récente dans la base de donnée CAC : {last_high}")
-session.close()
-
-# on crée une liste des éléments à ajouter à la base de données dans l'ordre chronologique
-committing_list = []
-for element in scraping_list_CAC:
-    if element[0] not in last_high or last_high is None:
-        committing_list.insert(0, element)
-    else:
-        break
-for element in committing_list:
-    print(element)
-
-# on ouvre une nouvelle session pour peupler la base de données avec les éléments de la liste précédente
-session = Session()
-for element in committing_list:
-    data = CAC(date=element[0], closing=element[1], opening=element[2], higher=element[3], lower=element[4])
-    session.add(data)
-session.commit()
-
-# ====================== update LVC ======================
-
-# on ouvre une session pour récupérer la date la plus récente de la base de données
-session = Session()
-last_high = str(session.query(LVC.date).order_by(LVC.id.desc()).first())
-print(f"La date la plus récente dans la base de donnée LVC : {last_high}")
-session.close()
-
-# on crée une liste des éléments à ajouter à la base de données dans l'ordre chronologique
-committing_list = []
-for element in scraping_list_LVC:
-    if element[0] not in last_high or last_high is None:
-        committing_list.insert(0, element)
-    else:
-        break
-for element in committing_list:
-    print(element)
-
-# on ouvre une nouvelle session pour peupler la base de données avec les éléments de la liste précédente
-session = Session()
-for element in committing_list:
-    data = LVC(date=element[0], closing=element[1], opening=element[2], higher=element[3], lower=element[4])
-    session.add(data)
-session.commit()
-
 
 def stocks_db_update(TABLE, scraping_list):
+    """
+    fonction qui met à jour une table de la base de données
+    :param TABLE: la TABLE à mettre à jour
+    :param scraping_list: la liste des éléments à insérer
+    :return: une TABLE de la BDD complétée avec les dernières données disponibles
+    """
     # on ouvre une session pour récupérer la date la plus récente de la base de données
     inner_session = Session()
     inner_last_high = str(inner_session.query(TABLE.date).order_by(TABLE.id.desc()).first())
-    print(f"La date la plus récente dans la base de donnée {TABLE}: {inner_last_high}")
+    print(f"La date la plus récente dans la base de donnée {TABLE.__tablename__}: {inner_last_high}")
     inner_session.close()
 
     # on crée une liste des éléments à ajouter à la base de données dans l'ordre chronologique
@@ -91,5 +49,7 @@ def stocks_db_update(TABLE, scraping_list):
     inner_session.close()
 
 
-# ====================== update BX4 ======================
+# ====================== update des Stocks scrapées ======================
+stocks_db_update(CAC, scraping_list_CAC)
+stocks_db_update(LVC, scraping_list_LVC)
 stocks_db_update(BX4, scraping_list_BX4)
