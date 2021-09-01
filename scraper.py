575234519e0cc4833b49c7fde41a34fbd7c4cf28
Locullus <bula.mickael@gmail.com>
2021-09-01 23:32:52 +0200

    add variables to simplify xpath

--- scraper.py
+++ scraper.py
@@ -4,12 +4,14 @@ from database import CAC, LVC, BX4
 
 # on crée un dictionnaire prenant en valeur les xpath à scraper
 base = '//*[@id="main-content"]/div/section[1]/div[2]/article/div[1]'
+CAC_base = '/div[2]/div[1]/div[5]/div[2]/div[1]/div/div/table'
+STOCKS_base = '/div[1]/div[1]/div[6]/div[2]/div[1]/div/div/table'
 
-CAC_xpath_dict = {'date_xpath': base + '/div[2]/div[1]/div[5]/div[2]/div[1]/div/div/table/thead/tr/th[{}]/text()',
-                  'row_xpath': base + '/div[2]/div[1]/div[5]/div[2]/div[1]/div/div/table/tbody/tr[{}]/td[{}]/text()'}
+CAC_xpath_dict = {'date_xpath': base + CAC_base + '/thead/tr/th[{}]/text()',
+                  'row_xpath': base + CAC_base + '/tbody/tr[{}]/td[{}]/text()'}
 
-STOCKS_xpath_dict = {'date_xpath': base + '/div[1]/div[1]/div[6]/div[2]/div[1]/div/div/table/thead/tr/th[{}]/text()',
-                     'row_xpath': base + '/div[1]/div[1]/div[6]/div[2]/div[1]/div/div/table/tbody/tr[{}]/td[{}]/text()'}
+STOCKS_xpath_dict = {'date_xpath': base + STOCKS_base + '/thead/tr/th[{}]/text()',
+                     'row_xpath': base + STOCKS_base + '/tbody/tr[{}]/td[{}]/text()'}
 
 # on scrape les données des valeurs CAC, LVC et BX4 pour mettre à jour la base de données
 cac_content = parsing_url('https://www.boursorama.com/bourse/indices/cours/1rPCAC/')
